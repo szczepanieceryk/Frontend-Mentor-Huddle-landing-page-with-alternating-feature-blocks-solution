33a52fcd2ed69577c1eb3cafac6c87eb861103cf
Eryk Szczepaniec <szczepanieceryk@gmial.com>
2022-02-07 17:10:12 +0100

    add styles for desktop

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+// header
+const headerTitle = document.querySelector('h2');
+const headerTxt = document.querySelector('span');
 // component and content fields
 const component = document.querySelectorAll('.component');
-const componentImage = document.querySelectorAll('.component-img')
-const componentTitle = document.querySelectorAll('.component-title')
-const componentText = document.querySelectorAll('.component-text')
+const componentImage = document.querySelectorAll('.component-img');
+const componentTitle = document.querySelectorAll('.component-title');
+const componentText = document.querySelectorAll('.component-text');
+// build comunity section
+const comunityTitle = document.getElementById('comunity-title')
+// bottom navigation
+const navLinks = document.getElementsByClassName('nav-link')
+
+// header context
+headerTitle.innerText = 'Build The Community Your Fans Will Love';
+headerTxt.innerText = 'Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.';
 
 // content for main section components
 const mainContent = [
@@ -29,5 +40,36 @@ mainContent.forEach(() => {
         componentImage[i].src = mainContent[i].image;
         componentTitle[i].innerText = mainContent[i].title;
         componentText[i].innerText = mainContent[i].text;
+    }
+})
+
+// build your comunity section
+comunityTitle.innerText = 'Ready to Build Your Comunity?'
+
+// bottom navigation
+const navigation = [
+    {
+        navLink: 'About Us'
+    },
+    {
+        navLink: 'What We Do'
+    },
+    {
+        navLink: 'FAQ'
+    },
+    {
+        navLink: 'Career'
+    },
+    {
+        navLink: 'Blog'
+    },
+    {
+        navLink: 'Contact Us'
+    }
+]
+
+navigation.forEach(() => {
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].innerText = navigation[i].navLink
     }
 })
